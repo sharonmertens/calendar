c0efee1422ffb9a336693e757b3b81c81442e048
Sharon Mertens <sharon.mertens@gmail.com>
2021-01-23 17:32:03 -0500

    first attempt fetch

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log('hello world');
+
+const apiKey = 'b2f9b657d8fd4c34a28beba13cab25c2​';
+
+fetch('https://api.bizzabo.com/api', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/vnd.bizzabo.v2.0+json',
+        'Authorization': 'bearer' + apiKey,
+    },
+    mode: 'cors',
+    cache: 'default',
+})
+    .then(respone => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error))
